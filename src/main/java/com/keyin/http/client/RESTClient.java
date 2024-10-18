987f102656af6b76dcb0771ecbcee0ad6cba49e2
Jordan <jordan.kelloway@keyin.com>
2024-10-18 13:09:34 -0230

    Small refactor of dupe code

--- src/main/java/com/keyin/http/client/RESTClient.java
+++ src/main/java/com/keyin/http/client/RESTClient.java
@@ -20,6 +20,19 @@ public class RESTClient {
     private String serverURL;
     private HttpClient client;
 
+    public String getServerURL() {
+        return serverURL;
+    }
+    public void setServerURL(String serverURL) {
+        this.serverURL = serverURL;
+    }
+    public HttpClient getClient() {
+        if (client == null) {
+            client  = HttpClient.newHttpClient();
+        }
+        return client;
+    }
+
     public String getResponseFromHTTPRequest() {
         String responseBody = "";
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
@@ -29,102 +42,63 @@ public class RESTClient {
             if (response.statusCode()!=200) {
                 System.out.println("Status Code: " + response.statusCode());
             }
-
             responseBody = response.body();
-
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
         return responseBody;
     }
 
-
     public List<Author> getAllAuthors() {
         List<Author> authors = new ArrayList<Author>();
-
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
-
         try {
-            HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode()==200) {
-                System.out.println("*****Response Body Print****");
-                System.out.println("***** " + response.body());
-            } else {
-                System.out.println("Error Status Code: " + response.statusCode());
-            }
-
+            HttpResponse<String> response = httpSender(request);
             authors = buildAirportListFromResponse(response.body());
-
-
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
-
         return authors;
     }
 
     public List<Author> buildAirportListFromResponse(String response) throws JsonProcessingException {
         List<Author> authors = new ArrayList<Author>();
-
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         authors = mapper.readValue(response, new TypeReference<List<Author>>(){});
-
         return authors;
     }
 
     public List<Book> buildBookListForAuthorFromResponse(String response) throws JsonProcessingException {
         List<Book> books = new ArrayList<Book>();
-
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         books = mapper.readValue(response, new TypeReference<List<Book>>(){});
-
         return books;
     }
 
 
-    public String getServerURL() {
-        return serverURL;
-    }
-
-    public void setServerURL(String serverURL) {
-        this.serverURL = serverURL;
-    }
-
-    public HttpClient getClient() {
-        if (client == null) {
-            client  = HttpClient.newHttpClient();
-        }
-
-        return client;
-    }
-
     public List<Book> getBooksForAuthor() {
-
         List<Book> books = new ArrayList<Book>();
-
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
-
         try {
-            HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode()==200) {
-                System.out.println("*****Response Body Print****");
-                System.out.println("***** " + response.body());
-            } else {
-                System.out.println("Error Status Code: " + response.statusCode());
-            }
+            HttpResponse<String> response = httpSender(request);
 
             books = buildBookListForAuthorFromResponse(response.body());
-
-
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
-
         return books;
+    }
+
+    private HttpResponse<String> httpSender(HttpRequest request) throws IOException, InterruptedException {
+        HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode()==200) {
+            System.out.println("*****Response Body Print****");
+            System.out.println("***** " + response.body());
+        } else {
+            System.out.println("Error Status Code: " + response.statusCode());
+        }
+        return response;
     }
 }
